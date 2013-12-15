a26c293857cabdc386bfeed6dc1fd5b61daca35b
Clint Checketts <clintchecketts@ldschurch.org>
2013-12-14 22:02:22 -0700

    Create GH Pages

---
+++ README.md
@@ -0,0 +1,4 @@
+#Bernardo and/or Douglas
+* Can I upload the splash screen from Illustrator? And to which file?
+
+
---
+++ javascript.js
@@ -0,0 +1,26 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json');
+
+var items;
+xhr.onreadystatechange = function () {
+  if (this.status == 200 && this.readyState == 4) {
+    items = JSON.parse(this.responseText);
+    addItems(items);
+  }
+};
+xhr.send();
+//test
+
+var outputEl = document.getElementById("output");
+
+var addItems = function(items){
+  outputEl.innerHTML = "";
+  for(var i=0;i<items.length;i++){
+//    outputEl.innerHTML += "<div>"+items[i].name+"</div>";
+    console.log(outputEl.innerHTML);
+    var itemEl = document.createElement('div');
+    itemEl.textContent = items[i].name;
+    
+    outputEl.appendChild(itemEl);
+  }
+};
