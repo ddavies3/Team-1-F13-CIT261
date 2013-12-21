d1fa050c0998a19bff1a633727069857dad3b3be
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 16:28:29 -0700

    test

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -6,18 +6,3 @@ lsit.chicken = 2;
 console.log(list);
 localStorage.setItem('list',JSON.stringify(list));
 console.lolg(JSON.parse(LocalStorage.getItem('list')));
-//test
-
-/* var outputEl = document.getElementById("output");
-
-/* var addItems = function(items){
-  outputEl.innerHTML = "";
-  for(var i=0;i<items.length;i++){
-//    outputEl.innerHTML += "<div>"+items[i].name+"</div>";
-    console.log(outputEl.innerHTML);
-    var itemEl = document.createElement('div');
-    itemEl.textContent = items[i].name;
-    
-    outputEl.appendChild(itemEl);
-  }
-/* }; */ */ */
