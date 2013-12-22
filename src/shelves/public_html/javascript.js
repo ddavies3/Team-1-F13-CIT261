62ee0f2b1393ac7a1bc3d74c902a73f33b36c7f9
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 17:20:33 -0700

    t

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -1,9 +1,3 @@
-var list ={};
-list.beans=4;
-list.corn = 6;
-list.cereal = 20;
-list.chicken = 2;
-console.log(list);
-LocalStorage.setItem( 'list' , JSON.stringify(list) );
-console.log(JSON.parse(LocalStorage.getItem('list')));
-
+localStorage.setItem('favoriteflavor','vanilla');
+var taste = localStorage.getItem('favoriteflavor');
+console.output(taste);
