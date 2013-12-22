250b3224cb509259fdb199a84bb8a74b11b4d2de
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 17:30:03 -0700

    f

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -2,4 +2,4 @@ localStorage.setItem('favorite','vanilla');
 localStorage.setItem('favoriteflavor','chocolate');
 var taste = localStorage.getItem('favorite');
 var tast = localStorage.getItem('favoriteflavor');
-print.log(taste, tast);
+console.log(taste, tast);
