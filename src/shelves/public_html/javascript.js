5a665fa67f81505671c195d583feedf06546b08d
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 17:32:45 -0700

    d

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -3,3 +3,6 @@ localStorage.setItem('favoriteflavor','chocolate');
 var taste = localStorage.getItem('favorite');
 var tast = localStorage.getItem('favoriteflavor');
 console.log(taste, tast);
+
+if(localStorage && localStorage.getItem('taste)){
+render(JSON.parse(localStorage.getItem('taste)));
