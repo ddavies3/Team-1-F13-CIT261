437758f8e34fa2ceb02ffed5a07c998325819789
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 18:23:41 -0700

    d

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -7,4 +7,18 @@ console.log( JSON.parse( localStorage.getItem( 'list' ) ) );
 
 if(localStorage && localStorage.getItem('list')){
   render(JSON.parse(localStorage.getItem('list')));
+} else {
+  $('#list').html(''+loading+'');
+  var query = 'select centroid,woeid,name,boundingBox'+
+              ' from geo.places.children(0)'+
+              ' where parent_woeid=1 and placetype="country"'+
+              ' | sort(field="name")';
+  var YQL = 'http://query.yahooapis.com/v1/public/yql?q='+
+             encodeURIComponent(query)+'&diagnostics=false&format=json';
+  $.getJSON(YQL,function(data){
+    if(localStorage){
+      localStorage.setItem('thewholefrigginworld',JSON.stringify(data));
+    }
+    render(data);
+  });
 }
