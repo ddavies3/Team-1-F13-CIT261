4c47c4ced4d27f55881aecbf6b18a0b7aa65621a
Douglas <dougdaviesdesign@gmail.com>
2013-12-21 18:54:28 -0700

    d

--- src/shelves/public_html/javascript.js
+++ src/shelves/public_html/javascript.js
@@ -1,7 +1,7 @@
 var list = {};
-list.can = 'beans';
-list.bag = 'corn';
-list.sack = 'flour';
+list.canbean = 4;
+list.bagcorn = 6;
+list.sacksflour = 3;
 localStorage.setItem( 'list', JSON.stringify(list) );
 console.log( JSON.parse( localStorage.getItem( 'list' ) ) );
 
